0c558bdb6bdf6cbac3767335ed05f4add980036c
Andrei P <popandrei230@yahoo.com>
2024-05-21 19:09:28 +0300

    Increase CF cache TTL for fewer requests to S3

--- cloudfront.tf
+++ cloudfront.tf
@@ -34,7 +34,7 @@ resource "aws_cloudfront_distribution" "website_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
+    default_ttl            = 86400
     max_ttl                = 86400
 
     response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers_policy.id
